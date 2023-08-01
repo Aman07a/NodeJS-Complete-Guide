352fc927ab505a099a7ee6d9569a04f0ea44ec6a
Aman07a <aman16.aa17@gmail.com>
2023-08-01 14:43:26 +0200

    Working with Objects, Properties & Methods

--- play.js
+++ play.js
@@ -1,26 +1,9 @@
-const name = 'Aman';
-let age = 22;
-const hashHobbies = true;
-
-age = 23;
-
-const summarizeUser = (userName, userAge, userHobby) => {
-  return (
-    'Name is ' +
-    userName +
-    ', age is ' +
-    userAge +
-    ' and the user has hobbies: ' +
-    userHobby
-  );
+const person = {
+  name: 'Aman',
+  age: 22,
+  greet() {
+    console.log('Hi, I am ' + this.name);
+  },
 };
 
-// const add = (a, b) => a + b;
-// const addOne = (a) => a + 1;
-const addRandom = () => 1 + 2;
-
-// console.log(add(1, 2));
-// console.log(addOne(1));
-console.log(addRandom());
-
-console.log(summarizeUser(name, age, hashHobbies));
+person.greet();
