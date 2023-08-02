645fd14b05084b8fdfd6743d20eb503a751dd82a
Aman07a <aman16.aa17@gmail.com>
2023-08-02 18:00:28 +0200

    Changing Variables in the Debug Console

--- routes.js
+++ routes.js
@@ -30,7 +30,6 @@ const requestHandler = (req, res) => {
       });
     });
   }
-
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title><head>');
