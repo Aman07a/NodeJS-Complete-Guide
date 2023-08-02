d04d629c63066bf7b1706df85966b9a600985c8b
Aman07a <aman16.aa17@gmail.com>
2023-08-02 10:43:51 +0200

    Parsing Request Bodies

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
 
   if (url === '/') {
     res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
+    res.write('<head><title>Enter Message</title><head>');
     res.write(
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
@@ -16,7 +16,16 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
