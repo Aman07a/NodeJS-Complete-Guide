57c1f9d9a83899be34086e071eaa849ce6311fe7
Aman07a <aman16.aa17@gmail.com>
2023-08-02 14:20:15 +0200

    Global Features vs Core Modules vs Third-Party Modules

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const http = require('http');
 
 const routes = require('./routes');
 
+const sessions = require('express-session');
+
 console.log(routes.someText);
 
 const server = http.createServer(routes.handler);
