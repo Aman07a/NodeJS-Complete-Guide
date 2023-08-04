83409f7d76f7b5c450104063c52b2e3312930a96
Aman07a <aman16.aa17@gmail.com>
2023-08-04 09:04:59 +0200

    Working with Handlebars

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars').create({
+  extname: 'hbs',
+});
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', handlebars.engine);
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -19,8 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+app.use('**', (req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Not Found' });
 });
 
 app.listen(3000);
