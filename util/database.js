2e346c479c881311dc339522066542f25b1b30ba
Aman07a <aman16.aa17@gmail.com>
2023-08-10 08:48:35 +0200

    Fixed: Installing the MongoDB Driver

--- util/database.js
+++ util/database.js
@@ -1,19 +1,17 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-const url =
-  'mongodb+srv://aman16aa17:mongodb-nodejs@cluster0.ogqbk9c.mongodb.net/?retryWrites=true&w=majority';
-
-const connectToDatabase = (callback) => {
-  MongoClient.connect(url)
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://aman16aa17:mongodb-nodejs@cluster0.ogqbk9c.mongodb.net/?retryWrites=true&w=majority'
+  )
     .then((client) => {
       console.log('Connected to MongoDB');
-      const db = client.db(); // Access the database instance
-      callback(db); // Pass the db object to the callback
+      callback(client); // Pass the client object to the callback
     })
     .catch((err) => {
       console.error('Error connecting to MongoDB:', err);
     });
 };
 
-module.exports = connectToDatabase;
+module.exports = mongoConnect;
