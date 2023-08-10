fd40f14af2072d33df69fcbe9c24d0be44689b4f
Aman07a <aman16.aa17@gmail.com>
2023-08-10 08:39:01 +0200

    Setting Up MongoDB

--- util/database.js
+++ util/database.js
@@ -1,13 +1,19 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-MongoClient.connect('')
-  .then((result) => {
-    console.log('Connected');
-    callback(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+const url =
+  'mongodb+srv://aman16aa17:mongodb-nodejs@cluster0.ogqbk9c.mongodb.net/?retryWrites=true&w=majority';
 
-module.exports = MongoClient;
+const connectToDatabase = (callback) => {
+  MongoClient.connect(url)
+    .then((client) => {
+      console.log('Connected to MongoDB');
+      const db = client.db(); // Access the database instance
+      callback(db); // Pass the db object to the callback
+    })
+    .catch((err) => {
+      console.error('Error connecting to MongoDB:', err);
+    });
+};
+
+module.exports = connectToDatabase;
