84530fe196350e8babc275753697bbcc311d1aba
Aman07a <aman16.aa17@gmail.com>
2023-08-14 10:32:21 +0200

    Deleting a Cookie

--- app.js
+++ app.js
@@ -38,15 +38,6 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  User.findById('64d5edbe518312a4f2c1b10b')
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
-});
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
