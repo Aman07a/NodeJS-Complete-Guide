93f06f1ad008ac9b776865c6473e051867b57ba3
Aman07a <aman16.aa17@gmail.com>
2023-08-14 17:33:00 +0200

    Using a CSRF Token

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,7 +38,6 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
-        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => {
@@ -46,9 +45,10 @@ exports.getIndex = (req, res, next) => {
     });
 };
 
-exports.getCart = async (req, res, next) => {
-  await req.user
+exports.getCart = (req, res, next) => {
+  req.user
     .populate('cart.items.productId')
+    .execPopulate()
     .then((user) => {
       const products = user.cart.items;
       res.render('shop/cart', {
@@ -83,9 +83,10 @@ exports.postCartDeleteProduct = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 
-exports.postOrder = async (req, res, next) => {
-  await req.user
+exports.postOrder = (req, res, next) => {
+  req.user
     .populate('cart.items.productId')
+    .execPopulate()
     .then((user) => {
       const products = user.cart.items.map((i) => {
         return { quantity: i.quantity, product: { ...i.productId._doc } };
