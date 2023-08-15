10e3b11be91471507003bd448dacb867d43a9605
Aman07a <aman16.aa17@gmail.com>
2023-08-15 08:04:58 +0200

    Removed Csrf (Deprecated)

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
-const csrf = require('csurf');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -18,7 +17,6 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions',
 });
-const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -39,7 +37,6 @@ app.use(
     store: store,
   })
 );
-app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -51,12 +48,6 @@ app.use((req, res, next) => {
       next();
     })
     .catch((err) => console.log(err));
-});
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
 });
 
 app.use('/admin', adminRoutes);
