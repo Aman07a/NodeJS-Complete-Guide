c631cb9a9064d7d6afeb0bfca8805fc4d93424b1
Aman07a <aman16.aa17@gmail.com>
2023-08-16 18:28:59 +0200

    Filtering Files by Mimetype

--- app.js
+++ app.js
@@ -28,9 +28,24 @@ const fileStorage = multer.diskStorage({
     cb(null, 'images');
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
+    cb(
+      null,
+      new Date().toISOString().replace(/[-:TZ.]/g, '') + '-' + file.originalname
+    );
   },
 });
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -42,7 +57,9 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(multer({ storage: fileStorage }).single('image'));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
