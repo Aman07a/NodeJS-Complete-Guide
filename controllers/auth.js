7fa7523a9fc0f1407e29d5e7078fb7e8c860e254
Aman07a <aman16.aa17@gmail.com>
2023-08-16 08:35:25 +0200

    Using Validation Error Messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -84,13 +84,13 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  const errors = validationResult(req).array();
-  if (errors.length > 0) {
-    console.log(errors);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
     return res.status(422).render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors,
+      errorMessage: errors.array()[0].msg,
     });
   }
   User.findOne({ email: email })
