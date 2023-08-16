955a292bfcc6a65aced2f9cafdf1c8ca85ae39fc
Aman07a <aman16.aa17@gmail.com>
2023-08-16 08:27:49 +0200

    Fixed: Setup & Basic Validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 const dotenv = require('dotenv');
-const { validationResult } = require('express-validator/check');
+const { validationResult } = require('express-validator');
 
 dotenv.config();
 
@@ -84,13 +84,13 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    console.log(errors.array());
+  const errors = validationResult(req).array();
+  if (errors.length > 0) {
+    console.log(errors);
     return res.status(422).render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array(),
+      errorMessage: errors,
     });
   }
   User.findOne({ email: email })
