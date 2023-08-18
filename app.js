8fe0613ca0b7649751b60960b25649c5e1fc1024
Aman07a <aman16.aa17@gmail.com>
2023-08-18 10:16:00 +0200

    REST APIs, Clients & CORS Errors

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ const feedRoutes = require('./routes/feed');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use('/feed', feedRoutes);
 
