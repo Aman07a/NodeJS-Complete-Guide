09aaa1198176326e6f99904035909e2261832ad3
Aman07a <aman16.aa17@gmail.com>
2023-08-18 13:35:33 +0200

    Fixed Navigation

--- src/components/Navigation/MainNavigation/MainNavigation.js
+++ src/components/Navigation/MainNavigation/MainNavigation.js
@@ -11,7 +11,7 @@ const mainNavigation = (props) => (
   <nav className='main-nav'>
     <MobileToggle onOpen={props.onOpenMobileNav} />
     <div className='main-nav__logo'>
-      <NavLink to='/'>
+      <NavLink to='/' exact='true'>
         <Logo />
       </NavLink>
     </div>
