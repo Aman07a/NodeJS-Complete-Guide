c72599d2020fe4eecb089f90998c974a95a458f7
Aman07a <aman16.aa17@gmail.com>
2023-08-18 11:00:02 +0200

    Sending POST Requests

--- public/js/product.js
+++ public/js/product.js
@@ -7,3 +7,19 @@ getButton.addEventListener('click', () => {
     .then((resData) => console.log(resData))
     .catch((err) => console.log(err));
 });
+
+postButton.addEventListener('click', () => {
+  fetch('http://localhost:8080/feed/post', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'A Codepen Post',
+      content: 'Created via Codepen',
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((resData) => console.log(resData))
+    .catch((err) => console.log(err));
+});
