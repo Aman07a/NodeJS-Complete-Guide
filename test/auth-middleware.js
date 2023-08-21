1fd11671f6438a76535354c298eac5bfca7d766f
Aman07a <aman16.aa17@gmail.com>
2023-08-21 15:48:36 +0200

    What Not To Test!

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -14,12 +14,31 @@ describe('Auth middleware', function () {
     );
   });
 
-  it('should threw an error if the authorization header is only one string', function () {
+  it('should threw an error if the token can not be verified', function () {
     const req = {
       get: function (headerName) {
-        return 'xyz';
+        return 'Bearer xyz';
       },
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should threw an error if the token cannot be verified', function () {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer xyz';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should yield a userId after decoding the token', function () {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer dkfkalsdjfaslfjdlas';
+      },
+    };
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property('userId');
   });
 });
