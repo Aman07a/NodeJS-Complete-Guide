d414c493879bd23fa4d24dbe607c92f358068148
Aman07a <aman16.aa17@gmail.com>
2023-08-21 13:32:37 +0200

    Another Error: Fetching a Single Post

--- frontend/src/pages/Feed/SinglePost/SinglePost.js
+++ frontend/src/pages/Feed/SinglePost/SinglePost.js
@@ -14,29 +14,26 @@ class SinglePost extends Component {
 
   componentDidMount() {
     const postId = this.props.match.params.postId;
-    console.log('postid: ', postId);
-    fetch('http:://localhost:8080/feed/post/' + postId)
+
+    fetch('http://localhost:8080/feed/post/' + postId)
       .then((res) => {
-        console.log('res: ', res);
         if (res.status !== 200) {
           throw new Error('Failed to fetch status');
         }
-        console.log('in componentDidMount');
         return res.json();
       })
       .then((resData) => {
-        console.log('resData: ', resData);
         this.setState({
           title: resData.post.title,
           author: resData.post.creator.name,
+          image: 'http://localhost:8080/' + resData.post.imageUrl,
           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
           content: resData.post.content,
         });
       })
       .catch((err) => {
-        console.log('in componentDidMount catch block', err);
+        console.log(err);
       });
-    console.log('title', this.state.title);
   }
 
   render() {
@@ -56,3 +53,61 @@ class SinglePost extends Component {
 }
 
 export default SinglePost;
+
+// import React, { Component } from 'react';
+
+// import Image from '../../../components/Image/Image';
+// import './SinglePost.css';
+
+// class SinglePost extends Component {
+//   state = {
+//     title: '',
+//     author: '',
+//     date: '',
+//     image: '',
+//     content: '',
+//   };
+
+//   componentDidMount() {
+//     const postId = this.props.match.params.postId; // Use this.props.match.params
+//     console.log(postId);
+//     // const postId = this.props.match.params.postId;
+//     // const postId = '64e33c8b2e0b409d965ed04d';
+//     fetch('http://localhost:8080/feed/post/' + postId)
+//       .then((res) => {
+//         if (res.status !== 200) {
+//           throw new Error('Failed to fetch status');
+//         }
+//         return res.json();
+//       })
+//       .then((resData) => {
+//         this.setState({
+//           title: resData.post.title,
+//           author: resData.post.creator.name,
+//           image: 'http://localhost:8080/' + resData.post.imageUrl,
+//           date: new Date(resData.post.createdAt).toLocaleDateString('en-US'),
+//           content: resData.post.content,
+//         });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }
+
+//   render() {
+//     return (
+//       <section className='single-post'>
+//         <h1>{this.state.title}</h1>
+//         <h2>
+//           Created by {this.state.author} on {this.state.date}
+//         </h2>
+//         <div className='single-post__image'>
+//           <Image contain imageUrl={this.state.image} />
+//         </div>
+//         <p>{this.state.content}</p>
+//       </section>
+//     );
+//   }
+// }
+
+// export default SinglePost;
