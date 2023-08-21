1acb103804899bb2483203dbb1e220bc2018aa50
Aman07a <aman16.aa17@gmail.com>
2023-08-21 11:30:11 +0200

    Storing Posts in the Database

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { check, validationResult } = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -20,26 +22,28 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: 'Validation failed, entered data is incorrect.',
-        errors: errors.array(),
-      });
+    return res.status(422).json({
+      message: 'Validation failed, entered data is incorrect.',
+      errors: errors.array(),
+    });
   }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {
-        name: 'Aman',
-      },
-      createdAt: new Date(),
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/duck.jpg',
+    creator: { name: 'Aman' },
   });
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
