dd6d602976aa9abd998a6d0f0f553312db9b4e7d
Aman07a <aman16.aa17@gmail.com>
2023-08-21 16:11:27 +0200

    Using Stubs

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,4 +1,6 @@
 const expect = require('chai').expect;
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
 
 const authMiddleware = require('../middleware/is-auth');
 
@@ -14,19 +16,10 @@ describe('Auth middleware', function () {
     );
   });
 
-  it('should threw an error if the token can not be verified', function () {
+  it('should throw an error if the authorization header is only one string', function () {
     const req = {
       get: function (headerName) {
-        return 'Bearer xyz';
-      },
-    };
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
-  });
-
-  it('should threw an error if the token cannot be verified', function () {
-    const req = {
-      get: function (headerName) {
-        return 'Bearer xyz';
+        return 'xyz';
       },
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
@@ -35,10 +28,24 @@ describe('Auth middleware', function () {
   it('should yield a userId after decoding the token', function () {
     const req = {
       get: function (headerName) {
-        return 'Bearer dkfkalsdjfaslfjdlas';
+        return 'Bearer djfkalsdjfaslfjdlas';
       },
     };
+    sinon.stub(jwt, 'verify');
+    jwt.verify.returns({ userId: 'abc' });
     authMiddleware(req, {}, () => {});
     expect(req).to.have.property('userId');
+    expect(req).to.have.property('userId', 'abc');
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore();
+  });
+
+  it('should throw an error if the token cannot be verified', function () {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer xyz';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
   });
 });
