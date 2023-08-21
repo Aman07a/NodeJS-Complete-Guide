d7599c6d9af0d8fe9a5faecd2d9d5efc22182ed8
Aman07a <aman16.aa17@gmail.com>
2023-08-21 08:56:46 +0200

    Removed All Files

--- backup/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom';
-
-import './index.css';
-import App from './App';
-
-createRoot(document.getElementById('root')).render(<App />);
--- src/pages/Auth/Auth.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './Auth.css';
-
-const auth = (props) => (
-  <section className='auth-form'>{props.children}</section>
-);
-
-export default auth;
