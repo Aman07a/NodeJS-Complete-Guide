7494d7c33b359ef2ed60f2ee19351672832cd234
Aman07a <aman16.aa17@gmail.com>
2023-08-22 08:09:55 +0200

    Testing Code With An Active Database

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -5,28 +5,8 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 const AuthController = require('../controllers/auth');
 
-describe('Auth Controller - Login', function () {
-  it('should throw an error with code 500 if accessing the database fails', function (done) {
-    sinon.stub(User, 'findOne');
-    User.findOne.throws();
-
-    const req = {
-      body: {
-        email: 'test@test.com',
-        password: 'tester',
-      },
-    };
-
-    AuthController.login(req, {}, () => {}).then((result) => {
-      expect(result).to.be.an('error');
-      expect(result).to.have.property('statusCode', 500);
-      done();
-    });
-
-    User.findOne.restore();
-  });
-
-  it('should send a response with a valid user status for an existing user', function (done) {
+describe('Auth Controller', function () {
+  before(function (done) {
     mongoose
       .connect(
         'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test-messages?retryWrites=true'
@@ -37,10 +17,33 @@ describe('Auth Controller - Login', function () {
           password: 'tester',
           name: 'Test',
           posts: [],
+          _id: '5c0f66b979af55031b34728a',
         });
         return user.save();
       })
-      .then(() => {})
+      .then(() => {
+        done();
+      })
       .catch((err) => console.log(err));
   });
+});
+
+it('should throw an error with code 500 if accessing the database fails', function (done) {
+  sinon.stub(User, 'findOne');
+  User.findOne.throws();
+
+  const req = {
+    body: {
+      email: 'test@test.com',
+      password: 'tester',
+    },
+  };
+
+  AuthController.login(req, {}, () => {}).then((result) => {
+    expect(result).to.be.an('error');
+    expect(result).to.have.property('statusCode', 500);
+    done();
+  });
+
+  User.findOne.restore();
 });
