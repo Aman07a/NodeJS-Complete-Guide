1cb0596f50acfbb610165aca17d6f0214d03e9a1
Aman07a <aman16.aa17@gmail.com>
2023-08-23 11:01:02 +0200

    Working with Deno Permissions

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ const text = 'This is a test - and it should be stored in a file!';
 const encoder = new TextEncoder();
 const data = encoder.encode(text);
 
+// deno run --allow-write=message.txt app.ts
 Deno.writeFile('message.txt', data).then(() => {
   console.log('Wrote to file!');
 });
