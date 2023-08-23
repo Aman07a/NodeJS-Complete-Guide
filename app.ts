4dbac7a894e79dc353da65c6e369cfcc96ccdc2e
Aman07a <aman16.aa17@gmail.com>
2023-08-23 08:41:38 +0200

    Adding REST Routes with TypeScript

--- app.ts
+++ app.ts
@@ -1,5 +1,12 @@
 import express = require('express');
+import bodyParser = require('body-parser');
+
+import todosROuter from './routes/todos';
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use(todosROuter);
 
 app.listen(3000);
